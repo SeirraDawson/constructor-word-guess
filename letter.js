4021f933d8b0ef4d589a7f7b8d5080c38bf21d2b
Seirradawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-05-29 22:32:27 -0400

    minor updates

--- letter.js
+++ letter.js
@@ -21,13 +21,13 @@ function Letter (actual) {
         }
         return '_';
     };
-    //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
     this.checked = function (userGuess) {
         if (userGuess === this.actual) {
-            this.guessed = true;
+        //A function that takes a character as an argument and checks it against the underlying character, 
+        //updating the stored boolean value to true if it was guessed correctly
+        this.guessed = true;
         }
     }
 }
-
 
 module.exports = Letter;
